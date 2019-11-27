cc5ce3f8682d290c6d6d8b77843d1c01c7fd4c9c
Dalwinder Singh <dalwinder.95451996@gmail.com>
2019-11-27 18:54:14 -0500

    Initial Commit

---
+++ Lesson12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson12
+//
+//  Created by Student on 2019-11-27.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
